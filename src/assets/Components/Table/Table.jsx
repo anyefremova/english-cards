e60db9d5b4b758689a936783f3942033a5b002a5
anyefremova <anna.efremova.02@yandex.ru>
2023-12-25 00:49:35 +0500

    added a table structure with words

--- src/assets/Components/Table/Table.jsx
+++ src/assets/Components/Table/Table.jsx
@@ -3,8 +3,16 @@ import './Table.scss';
 export default function Table() {
 
     return (
-        <div class="table__section">
-            <h2 class="table__title">Слово</h2>
+        <div class="table__words">
+            <div class="table__hedings">
+                <div class="table__headings__section">Слово</div>
+                <div class="table__headings__section">Перевод</div>
+                <div class="table__headings__section">Транскрипция</div>
+                <div class="table__headings__section">Редактирование</div>
+            </div>
+            <div class="table__section">
+
+            </div>
         </div>
     )
 }
