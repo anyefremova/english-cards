1a9440c7ee2b1a93dce44dc78ee5b4980c104fe1
anyefremova <anna.efremova.02@yandex.ru>
2024-02-11 04:46:02 +0500

    added an icon

--- src/assets/Components/Card/Card.jsx
+++ src/assets/Components/Card/Card.jsx
@@ -3,11 +3,15 @@ import './Card.scss';
 export default function Card(props) {
     return (
         <div className='card__container'>
-            <div className='card__main'>
-                <div className='card__word'>{props.word}</div>
-                <div className='card__transcription'>{props.transcription}</div>
+            <div className='card__structure'>
+                <div className='card__main'>
+                    <div className='card__word'>{props.word}</div>
+                    <div className='card__transcription'>{props.transcription}</div>
+                </div>
+                <div className='card__buttob'>
+                    <button className='check__translate'>Проверить</button>
+                </div>
             </div>
-            <div className='button__check_translate'></div>
         </div>
     )
 }
