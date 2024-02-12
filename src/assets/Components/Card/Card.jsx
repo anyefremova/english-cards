21791ce09d102ec893a150a43b12fe68309b42c7
anyefremova <anna.efremova.02@yandex.ru>
2024-02-13 00:40:54 +0500

    added the logo to the header

--- src/assets/Components/Card/Card.jsx
+++ src/assets/Components/Card/Card.jsx
@@ -26,19 +26,3 @@ export default function Card(props) {
         </div>
     );
 }
-
-/*export default function Card(props) {
-    return (
-        <div className='card__container'>
-            <div className='card__structure'>
-                <div className='card__main'>
-                    <div className='card__word'>{props.word}</div>
-                    <div className='card__transcription'>{props.transcription}</div>
-                </div>
-                <div className='card__button'>
-                    <button className='check__translate'>Проверить</button>
-                </div>
-            </div>
-        </div>
-    )
-}*/
