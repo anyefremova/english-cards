89adb83e4904a5ee92fd4b4a74edfc753503d085
anyefremova <anna.efremova.02@yandex.ru>
2024-02-14 03:10:48 +0500

    added the logo to the header

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import './Header.scss';
+import logoDark from '../../images/logoDark.png';
+import logoLight from '../../images/logoLight.png';
 
 export default function Header() {
 
     return (
         <div className='header'>
             <h3 className='header__logo'>WordWings</h3>
+            <img src={logoDark} alt="English Easy to Learn" className='header__logo_img' />
             <div className='header__menu'>
                 <p className='header__title'>Список слов</p>
                 <p className='header__title'>Карточки</p>
