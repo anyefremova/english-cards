94dbe78e8d25ed6ac70ce4f0772a47127922d7b0
anyefremova <anna.efremova.02@yandex.ru>
2024-02-15 02:55:02 +0500

    added routing

--- src/assets/Components/App.jsx
+++ src/assets/Components/App.jsx
@@ -3,18 +3,32 @@ import Header from './Header/Header.jsx';
 import Main from './Main/Main.jsx';
 import Footer from './Footer/Footer.jsx';
 import TableWithWords from './TableWithWords/TableWithWords.jsx';
-import CardList from './CardList/Cardlist.jsx';
+import CardList from './CardList/CardList.jsx';
+import wordsData from '../data/words.json';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
 
 function App() {
 
     return (
-        <div className='app'>
-            <Header />
-            <Main />
-            <TableWithWords />
-            <CardList />
-            <Footer />
-        </div >
+        <>
+            <Router>
+                <div className='app'>
+                    <Header />
+
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/tablewithwords" element={<TableWithWords />} />
+                        <Route path="/cardlist" element={<CardList wordsData={wordsData} />} />
+                    </Routes>
+
+                    <Footer />
+                </div >
+            </Router>
+        </>
     )
 }
 
--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import './Header.scss';
 import logoDark from '../../images/logoDark.png';
-import logoLight from '../../images/logoLight.png';
+import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 export default function Header() {
 
     return (
-        <div className='header'>
-            <h3 className='header__logo'>WordWings</h3>
-            <img src={logoDark} alt="English Easy to Learn" className='header__logo_img' />
-            <div className='header__menu'>
-                <p className='header__title'>Список слов</p>
-                <p className='header__title'>Карточки</p>
+        <header className='header'>
+            <div className='header__logo'>
+                <h3 className='header__title'>WordWings</h3>
+                <img src={logoDark} alt="English Easy to Learn" className='header__logo_img' />
+                <div className='header__menu'>
+                    <ul>
+                        <li className='menu__item'><NavLink to="/">Главная страница</NavLink></li>
+                        <li className='menu__item'><NavLink to="/tablewithwords">Список слов</NavLink></li>
+                        <li className='menu__item'><NavLink to="/cardlist"></NavLink>Карточки</li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </ header>
     )
 }
