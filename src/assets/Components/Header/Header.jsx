7453966f12d76264b2680d8574dd56674c2f720e
anyefremova <anna.efremova.02@yandex.ru>
2024-02-16 03:57:27 +0500

    fixed the header and added styles to some components

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,15 +1,32 @@
 import './Header.scss';
 import logoDark from '../../images/logoDark.png';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <header className='header'>
+        <header className={isScrolled ? 'header header__scrolled' : 'header'}>
             <div className='header__logo'>
                 <NavLink to="/"><h3 className='header__title'>WordWings</h3></NavLink>
-                <NavLink to="/"><img src={logoDark} alt="English Easy to Learn" className='header__logo_img' /></NavLink>
+                <NavLink to="/"><img src={logoDark} alt="Word wings logo" className='header__logo_img' /></NavLink>
             </div>
             <div className='header__menu'>
                 <ul className='menu__list'>
--- src/assets/Components/Main/Main.jsx
+++ src/assets/Components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import './Main.scss';
+import logoDark from '../../images/logoDark.png';
 
 export default function Main() {
 
     return (
         <div className='main'>
             <h2 className='main__title'>Elevate Your English, Master Every Word!</h2>
+            <img src={logoDark} alt="Word Wings logo" className='main__logo' />
+            <p>Здесь будет кнопка "Начать изучение"</p>
         </div>
     )
 }
