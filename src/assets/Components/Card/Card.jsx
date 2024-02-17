84ff84ccc695ba2c2710dbb4a102e1cd9f3ec69f
anyefremova <anna.efremova.02@yandex.ru>
2024-02-17 23:59:19 +0500

    destructured the card

--- src/assets/Components/Card/Card.jsx
+++ src/assets/Components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import './Card.scss';
 import { useState } from 'react';
 
-export default function Card(props) {
+export default function Card({ word, transcription, translate }) {
     const [showTranslate, setShowTranslate] = useState(false);
 
     const toggleTranslate = () => {
@@ -12,11 +12,11 @@ export default function Card(props) {
         <div className='card__container'>
             <div className='card__structure'>
                 <div className='card__main'>
-                    <div className='card__word'>{props.word}</div>
-                    <div className='card__transcription'>{props.transcription}</div>
+                    <div className='card__word'>{word}</div>
+                    <div className='card__transcription'>{transcription}</div>
                 </div>
                 {showTranslate ? (
-                    <div className='card__translate'>{props.translate}</div>
+                    <div className='card__translate'>{translate}</div>
                 ) : (
                     <div className='card__button'>
                         <button className='check__translate' onClick={toggleTranslate}>Показать перевод</button>
