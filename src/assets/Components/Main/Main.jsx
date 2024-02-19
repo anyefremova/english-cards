b7bc2866458c17cd19385844b9a5fecc95267daa
anyefremova <anna.efremova.02@yandex.ru>
2024-02-19 16:07:08 +0500

    added a button to the main page

--- src/assets/Components/Main/Main.jsx
+++ src/assets/Components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import './Main.scss';
 import logoDark from '../../images/logoDark.png';
+import { NavLink } from 'react-router-dom';
 
 export default function Main() {
 
@@ -7,7 +8,7 @@ export default function Main() {
         <div className='main'>
             <h2 className='main__title'>Elevate Your English, Master Every Word!</h2>
             <img src={logoDark} alt="Word Wings logo" className='main__logo' />
-            <p>Здесь будет кнопка "Начать изучение"</p>
+            <NavLink to="/cardlist"><button className='main__button'>Начать изучение</button></NavLink>
         </div>
     )
 }
